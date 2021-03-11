b76236c3c3cc417c6127082fd56e0e78ccc41f22
Angel Daniel Aviles cruz <angeldaniel.avilescruz66@gmail.com>
2021-03-11 00:08:16 -0600

    validar token con GUARDS

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { of } from "rxjs";
+import { of, Observable } from 'rxjs';
 import { catchError, map, tap } from "rxjs/operators";
 
 import { environment } from '../../../environments/environment';
@@ -40,14 +40,26 @@ export class AuthService {
     );
   }
 
-  validarToken(){
+  validarToken(): Observable<boolean>{
 
     const url = `${this.baseUrl}/auth/renew`;
     const headers = new HttpHeaders()
         .set('x-token', localStorage.getItem('token') || '');
 
     // return this.http.get( url, { headers : headers } );
-    return this.http.get( url, { headers } );
+    return this.http.get<AuthResponse>( url, { headers } )
+                    .pipe(
+                      map( resp => {
+                        console.log(resp.token);                        
+                        localStorage.setItem('token', resp.token! )
+                        this._usuario = {
+                          name: resp.name!,
+                          uid: resp.uid!
+                        }
+                        return resp.ok;
+                      }),
+                      catchError( err => of(false) )
+                    )
 
   }
 }
